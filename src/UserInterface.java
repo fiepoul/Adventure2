d694065a97f5465051b164234cf054b0e445199e
fiepoul <fipo0001@stud.kea.dk>
2023-11-02 16:21:33 +0100

    fourth commit

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,17 +1,13 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class UserInterface {
 
     private final Adventure adventure;
-    private final Pattern commandPattern;
     private final Player player;
 
     public UserInterface(Adventure adventure) {
         this.adventure = adventure;
         this.player = adventure.getPlayer();
-        this.commandPattern = Pattern.compile("^(go (north|south|east|west)|look|inventory|take\\s\\w+|drop\\s\\w+|equip\\s\\w+|attack|help|exit|health|eat\\s\\w+|)$");
     }
 
     public void start() {
@@ -21,14 +17,8 @@ public class UserInterface {
 
         while (gameRunning) {
             String input = scanner.nextLine().toLowerCase();
-            if (validateCommand(input)) {
-                gameRunning = handleCommand(input);
-            } else {
-                System.out.println("Invalid command. Type 'help' for available commands.");
-            }
+            gameRunning = handleCommand(input);
         }
-        System.out.println("Thank you for playing. Goodbye!");
-        scanner.close();
     }
 
     private void printWelcomeMessage() {
@@ -38,17 +28,17 @@ public class UserInterface {
         System.out.println("What would you like to do? Type 'help' to see your options: ");
     }
 
-    private boolean validateCommand(String input) {
-        Matcher matcher = commandPattern.matcher(input);
-        return matcher.matches();
-    }
-
     private boolean handleCommand(String input) {
         String[] commandParts = input.split(" ", 2);
         String command = commandParts[0];
         String argument = commandParts.length > 1 ? commandParts[1] : "";
 
         CommandHandler commandHandler = new CommandHandler(adventure, player);
+
+        if (command.equals("exit")) {
+            return commandHandler.exitGame();
+        }
+
         switch (command) {
             case "look" -> commandHandler.look();
             case "inventory" -> commandHandler.showInventory();
@@ -57,7 +47,6 @@ public class UserInterface {
             case "equip" -> commandHandler.equipWeapon(argument);
             case "attack" -> commandHandler.attackEnemy();
             case "help" -> commandHandler.printHelp();
-            case "exit" -> commandHandler.exitGame();
             case "take" -> commandHandler.takeItem(argument);
             case "drop" -> commandHandler.dropItem(argument);
             case "eat" -> commandHandler.eatFood(argument);
